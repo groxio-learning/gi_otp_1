163e0ed3bd2b28a6c7e601e1774e481656522f30
ebenedetti-futuring <ettore.benedetti92@gmail.com>
2024-04-29 14:48:37 +0200

    + life game

--- life/lib/life/application.ex
+++ life/lib/life/application.ex
@@ -1,4 +1,4 @@
-defmodule Rover.Application do
+defmodule Life.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
@@ -8,14 +8,13 @@ defmodule Rover.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Rover.Worker.start_link(arg)
-      # {Rover.Worker, arg}
-      # {Rover.Worker, []}
+      # Starts a worker by calling: Life.Worker.start_link(arg)
+      # {Life.Worker, arg}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Rover.Supervisor]
+    opts = [strategy: :one_for_one, name: Life.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
---
+++ life/lib/life/cell.ex
@@ -0,0 +1,15 @@
+defmodule Life.Cell do
+
+  def neightboards do
+
+  end
+
+  def show(true) do
+    "[ * ]"
+  end
+
+  def show(false) do
+    "[   ]"
+  end
+
+end
